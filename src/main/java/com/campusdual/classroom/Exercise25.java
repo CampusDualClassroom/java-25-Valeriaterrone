f830a4b0607b3aeb207e608772e78a05e526928d
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2025-01-13 15:11:50 +0100

    ejercicio listo y tests pasados :)

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-        Stack<String> stack = new Stack<>(); // Crear la pila
+        Stack<String> stack = new Stack<>(); // Crear la pila y luego agrego los slementos
         stack.push("Smith");
         stack.push("Montessori");
         stack.push("Peralta");
@@ -13,12 +13,13 @@ public class Exercise25 {
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+    while (!stack.empty()) {     //recorremos la pila mientras no este vacia
+        System.out.println(stack.pop());  // el pop es para obtener el elemento de la cima de la pila
+      }
     }
 
     public static void main(String[] args) {
-
+    Stack<String> stack = createStack();
+    printAndEmptyStack(stack);  // para imprimir y vaciar la pila
     }
-
-
 }
